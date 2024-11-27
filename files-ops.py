b99c2f2261430cd9bb8edef0b6b54937960e68a5
anvith-aki <75977318+anvith-aki@users.noreply.github.com>
2024-11-27 17:52:19 +0000

    Added Code

--- files-ops.py
+++ files-ops.py
@@ -1,54 +1,68 @@
 import os
 import sys
 
+
 def create_directory(dir_name):
-    # ... 
+    os.mkdir(dir_name)
+    print("Created Dir: ", dir_name)
     return
+
 
 def change_directory(dir_name):
-    # ... 
+    os.chdir(dir_name)
+    print("Changed to Dir: ", dir_name)
     return
+
 
 def delete_directory(dir_name):
-    # ...
+    os.rmdir(dir_name)
+    print("Deleted Dir: ", dir_name)
     return
+
 
 def list_dir():
-    # ...
+    print("Listing ---")
+    print(os.listdir())
     return
+
 
 def display_pwd():
-    # ... 
+    print("Current Dir: ", os.getcwd())
     return
 
+
 def create_file(file_name):
-    
+
     with open(file_name, 'w') as f:
         pass
     print(f"File '{file_name}' is created")
-     
+
     return
+
 
 def write_file(file_name):
-    
-    contents = input("Enter the contents into the file");
+
+    contents = input("Enter the contents into the file")
     with open(file_name, 'a') as f:
-        f.write(contents + '\n');
+        f.write(contents + '\n')
     print(f"Contents written to file successfully")
-     
+
     return
+
 
 def read_file(file_name):
-    
+
     with open(file_name, 'r') as f:
-        print(f.read());
+        print(f.read())
 
     return
+
 
 def delete_file(file_name):
-    
-    os.remove(file_name) 
+
+    os.remove(file_name)
     return
+
 
 def main():
     while True:
@@ -56,25 +70,43 @@ def main():
         print("2. Write to a file")
         print("3. Read a file")
         print("4. Delete a file")
+        print("5. Create a dir")
+        print("6. Delete a dir")
+        print("7. Change to dir")
+        print("8. list dir")
+        print("9. Current Work Dir")
         print("0. Exit")
         choice = input("Enter your choice here: ")
 
         if choice == '1':
-            file_name = input("1. Enter the file name")
+            file_name = input("1. Enter the file name: ")
             create_file(file_name)
         elif choice == '2':
-            file_name = input("2. Enter the file name")
+            file_name = input("2. Enter the file name: ")
             write_file(file_name)
         elif choice == '3':
-            file_name = input("3. Enter the file name")
+            file_name = input("3. Enter the file name: ")
             read_file(file_name)
         elif choice == '4':
-            file_name = input("4. Enter the file name")
+            file_name = input("4. Enter the file name: ")
             delete_file(file_name)
+        elif choice == '5':
+            dir_name = input("5. Enter the dir name: ")
+            create_directory(dir_name)
+        elif choice == '6':
+            dir_name = input("6. Enter the dir name: ")
+            delete_directory(dir_name)
+        elif choice == '7':
+            dir_name = input("7. Enter the dir name: ")
+            change_directory(dir_name)
+        elif choice == '8':
+            list_dir()
+        elif choice == '9':
+            display_pwd()
         elif choice == '0':
             break
         else:
-            print("Invalid choice");
+            print("Invalid choice")
 
 
 if __name__ == "__main__":
--- search.py
+++ search.py
@@ -1,37 +1,53 @@
 import re
 
+
 def grep(pattern, file_name):
     with open(file_name, 'r') as fp:
         for line in fp:
             if re.search(pattern, line):
                 print(line.strip())
-    return 
+    return
 
-def sed(old_pattern, new_patten, file_name):
-    # ... 
-    return 
+
+def sed(old_pattern, new_pattern, file_name):
+    with open(file_name, 'r') as fp:
+        lines = fp.readlines()
+    with open(file_name, 'w') as fp:
+        for line in lines:
+            updated_line = re.sub(old_pattern, new_pattern, line)
+            fp.write(updated_line)
+
+    print("Pattern replaced successfully.")
+    return
+
 
 def awk(n, file_name):
-    # ...
-    return 
+    with open(file_name, 'r') as fp:
+        for line in fp:
+            columns = line.strip().split()
+            if n <= len(columns):
+                print(columns[n - 1])  # To Convert 1-based index to 0-based
+    return
+
 
 def main():
-    file_name = input("Enter the file name")
-    command  = input("Enter the command: grep, sed, awk")
+    file_name = input("Enter the file name: ")
+    command = input("Enter the command - grep, sed, awk: ")
 
     if command == 'grep':
-        pattern = input("Enter the pattern")
-        grep(pattern, file_name);
+        pattern = input("Enter the pattern: ")
+        grep(pattern, file_name)
     elif command == 'sed':
-        old_pattern = input("Enter old pattern")
-        new_pattern = input("Enter new pattern")
+        old_pattern = input("Enter old pattern: ")
+        new_pattern = input("Enter new pattern: ")
         sed(old_pattern, new_pattern, file_name)
     elif command == 'awk':
-        n = input("Enter the column number")
+        n = input("Enter the column number: ")
         num = int(n)
-        awk(n, file_name)
+        awk(num, file_name)
     else:
-        print("Comamnd is invalid")
+        print("Command is invalid")
+
 
 if __name__ == "__main__":
     main()
